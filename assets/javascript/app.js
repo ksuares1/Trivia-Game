c5784e21b8a8233d880d2c55d64a7ce178bae2d3
Kimberly Suares <kimberlyjsuares@gmail.com>
2019-11-23 10:31:03 -0500

    adding functions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,18 @@
 $(document).ready(function() {
 
+    //Since the players should not see the answers before they play, lets hide them first. 
+    ('.answers').hide();
+   
     //the game will start at 60 seconds
     var number= 60;
     var intervalID;
+    var correctCount=0;
+    var wrongCount=0;
+    var unanswered=0;
     
+//Functions
+function showSummary(){
+    (".get-answers").show();
+}
 
 });
